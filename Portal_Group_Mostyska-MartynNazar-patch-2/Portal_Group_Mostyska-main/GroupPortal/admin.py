e1696ba3d39662cc84ce22fdf9b25ecb9811e034
MartynNazar <martynnazar11@gmail.com>
2026-09-04 20:08:05 +0300

    Delete Portal_Group_Mostyska-MartynNazar-patch-2 directory

--- Portal_Group_Mostyska-MartynNazar-patch-2/Portal_Group_Mostyska-main/GroupPortal/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import ForumTopic, ForumPost
-
-admin.site.register(ForumTopic)
-admin.site.register(ForumPost)
--- Portal_Group_Mostyska-MartynNazar-patch-2/Portal_Group_Mostyska-main/GroupPortal/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GroupportalConfig(AppConfig):
-    name = 'GroupPortal'
